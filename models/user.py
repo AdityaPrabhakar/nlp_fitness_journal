adb94dbfe022390d16c2a95c1797dd2ac7f8680a
Aditya Prabhakar <aprabhakar@clari.com>
2025-06-06 16:27:42 -0700

    increase password hash

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     display_name = db.Column(db.String(80))  # Public-facing name
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)  # ✅ increased size
 
     # American units
     bodyweight = db.Column(db.Float, nullable=True)  # In pounds (lbs)
